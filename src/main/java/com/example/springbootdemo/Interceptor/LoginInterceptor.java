f6d9d8ac1edb2af0cd695f5481de5a2ec0f8d18f
Yu Yang <ylctyuy@gmail.com>
2023-10-30 13:18:52 -0700

    interceptor part

---
+++ src/main/java/com/example/springbootdemo/Interceptor/LoginInterceptor.java
@@ -0,0 +1,14 @@
+package com.example.springbootdemo.Interceptor;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+public class LoginInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        System.out.println("LoginInterceptor");
+        return false;
+    }
+}
